a1a5acc1793ff73ca36fc9c985279b463ed8aab8
balakiranv <balakiran.@gmail.com>
2019-04-08 12:49:02 +0530

    Updates

---
+++ precache-manifest.1bf704eec9e71f542cbf77af031e3c9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "84a5c165761f595b6e36",
+    "url": "/Customer-support-react/static/css/main.2dab427e.chunk.css"
+  },
+  {
+    "revision": "84a5c165761f595b6e36",
+    "url": "/Customer-support-react/static/js/main.7686ea5c.chunk.js"
+  },
+  {
+    "revision": "686276929c7fd2ef3f55",
+    "url": "/Customer-support-react/static/js/runtime~main.f97118d8.js"
+  },
+  {
+    "revision": "65536e11561a75ed61a9",
+    "url": "/Customer-support-react/static/css/2.6d1bd0da.chunk.css"
+  },
+  {
+    "revision": "65536e11561a75ed61a9",
+    "url": "/Customer-support-react/static/js/2.03bc0497.chunk.js"
+  },
+  {
+    "revision": "d9f5c11af0db581f68329c6694926feb",
+    "url": "/Customer-support-react/index.html"
+  }
+];
